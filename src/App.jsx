f71d226c859e311a665014cef78df50e6e3f0166
Chris Lowe <>
2021-04-27 15:16:03 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <main>
+      <h1>THIS SITE HAS BEEN CONDEMNED</h1>
+    </main>
+  );
+}
+
+export default App;
